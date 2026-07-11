b66b9838be664410612a375cd9d1d81a59442a7b
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-07-11 12:04:52 +0700

    feat(MotionBox): add animated Box component using Framer Motion

---
+++ src/components/MotionBox.jsx
@@ -0,0 +1,35 @@
+import Box from "@mui/material/Box";
+import { motion } from 'framer-motion'; 
+
+/**
+ * @typedef {import('@mui/material').BoxProps & import('framer-motion').MotionProps} MotionBoxProps
+ */
+
+/**
+ * A Box component that supports animations using Framer Motion.
+ *
+ * @param {MotionBoxProps} props - Props for both the MUI Box component and Framer Motion.
+ */
+const MotionBox = ({ 
+    children, 
+    initial, 
+    whileInView, 
+    viewport, 
+    transition, 
+    ...restProps 
+}) => {
+    return (
+        <Box
+            component={motion.div} 
+            initial={{ opacity: 0, y: 100, ...initial }}
+            whileInView={{ opacity: 1, y: 0, ...whileInView }} 
+            viewport={{ margin: "100% 0px -100px 0px", ...viewport }} 
+            transition={{ duration: 0.7, ease: [0.25, 0.46, 0.45, 0.94], ...transition }} 
+            {...restProps}
+        >
+            {children}
+        </Box>
+    )
+};
+
+export default MotionBox;
