2b99f6096e3cf7905ba8428c3954772feb875913
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-07-14 19:17:29 +0700

    feat(projects): refactor project data structure and import from separate file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,58 +8,7 @@ import {
 import SectionHeading from './SectionHeader';
 import MotionBox from './MotionBox';
 import ProjectCard from './ProjectCard';
-
-const projects = [
-  {
-    slug: 'it-hub',
-    category: 'Website / E-Learning',
-    title: 'IT Hub',
-    description:
-      'A modern e-learning platform designed for IT student, with AI-powered learning assistant.',
-    image: '/projectImages/it-hub/home.png',
-    liveDemo: 'https://ithub.vercel.app/',
-    tags: [
-      'Next.js',
-      'Node.js',
-      'Express.js',
-      'PostgreSQL',
-      'Material UI',
-      'Gemini API',
-    ],
-  },
-  {
-    slug: 'gtg-shop',
-    category: 'Web & Mobile / E-Commerce',
-    title: 'GTG Shop',
-    description:
-      'A cross-platform e-commerce solution for electronic devices with secure payments and real-time features.',
-    image: '/projectImages/gtg-shop/customers/home.png',
-    liveDemo: 'https://gtgshop.vercel.app/',
-    tags: [
-      'ASP.NET Core',
-      'React',
-      'PostgreSQL',
-      'SignalR',
-      'Jetpack Compose',
-      'Material UI',
-    ],
-  },
-  {
-    slug: 'game-rc',
-    category: 'Website / Recomendation System',
-    title: 'Game Recommendation System (2024)',
-    description:
-      'A demo website that provides recommendations for related games as users browse through different games.',
-    image: '/projectImages/game-rc/home.png',
-    liveDemo: 'https://gamers.pythonanywhere.com/',
-    tags: [
-      'Django',
-      'Numpy',
-      'Pandas',
-      'Scikit-learn',
-    ],
-  },
-];
+import { projects } from '../data/projects';
 
 export default function Projects() {
   return (
