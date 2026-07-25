71388c84b29e1a5956ca2941f0a58f2d48cfd850
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-07-25 22:10:02 +0700

    chore(hero): update job title to software developer

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ export function Hero() {
         <Box id="hero" component={motion.section} py={10} display="flex" flexDirection={{ xs: 'column', md: 'row' }} alignItems="center" gap={6}>
             <Box flex={1}>
                 <Typography color="primary" component='h1' variant="h3" fontWeight="bold">Nguyễn Hữu Bảo Trung</Typography>
-                <Typography variant="h6" component='p' fontWeight={400} mt={2}>Web Developer Intern — Focused on React, Node.js & ASP.NET Core</Typography>
+                <Typography variant="h6" component='p' fontWeight={400} mt={2}>Software Developer Intern — Focused on React, Node.js & ASP.NET Core</Typography>
                 <Stack direction="row" spacing={2} mt={4}>
                     <Button
                         startIcon={<FileDownloadIcon />}
