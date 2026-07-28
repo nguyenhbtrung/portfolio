79ac6979a589ab1a8081768a8c6856b9a33befa4
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-07-28 22:08:32 +0700

    feat(projects): implement expandable section for archived projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
+  Button,
+  Collapse,
   Grid,
+  Stack,
   Typography,
 } from '@mui/material';
 // import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMore';
+
 import SectionHeading from '../SectionHeading';
 import MotionBox from '../MotionBox';
 import { ProjectCard } from './ProjectCard';
-import { projects } from '../../data/projects';
+import { ProjectArchiveCard } from './ProjectArchiveCard';
+import { projects, archiveProjects, } from '../../data/projects';
 
 export function Projects() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <Box
       id="projects"
@@ -47,6 +55,82 @@ export function Projects() {
           </Grid>
         ))}
       </Grid>
+
+      <Box mt={14}>
+        <Stack
+          spacing={1}
+          alignItems="center"
+          mb={6}
+        >
+          <Button
+            disableRipple
+            onClick={() => setExpanded((v) => !v)}
+            sx={{
+              textTransform: 'uppercase',
+              letterSpacing: 2,
+              color: 'text.secondary',
+
+              '&:hover': {
+                bgcolor: 'transparent',
+                color: 'text.primary',
+              },
+            }}
+          >
+            <Stack
+              direction="row"
+              spacing={2}
+              alignItems="center"
+            >
+              <Box
+                sx={{
+                  width: 40,
+                  height: 1,
+                  bgcolor: 'primary.main',
+                  opacity: .4,
+                }}
+              />
+
+              View More Projects
+
+              <Box
+                sx={{
+                  width: 40,
+                  height: 1,
+                  bgcolor: 'primary.main',
+                  opacity: .4,
+                }}
+              />
+            </Stack>
+          </Button>
+
+          <ExpandMoreRoundedIcon
+            sx={{
+              color: 'primary.main',
+              opacity: .6,
+              transition: '.3s',
+              transform: expanded
+                ? 'rotate(180deg)'
+                : 'rotate(0)',
+            }}
+          />
+        </Stack>
+
+        <Collapse in={expanded}>
+          <Grid container spacing={3}>
+            {archiveProjects.map((project) => (
+              <Grid
+                key={project.slug}
+                size={{
+                  xs: 12,
+                  lg: 6,
+                }}
+              >
+                <ProjectArchiveCard project={project} />
+              </Grid>
+            ))}
+          </Grid>
+        </Collapse>
+      </Box>
     </Box>
   );
 }
